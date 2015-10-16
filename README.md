0dcb1c3dd0fb42b3836cf74f449ee5dc37f70939
Jono Bacon <jono@jonobacon.org>
2015-10-16 10:01:39 -0700

    Complete rewrite. Broke the functionality out into different command line switches.

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
 # eventbrite-tools
 
-This is a simple script for getting registration data from Eventbrite into some kind of useful form. Thanks to Van Riper from Google for writing the initial version.
+This is a simple script for making use of attendee data from the Eventbrite event
+organization website.
 
-More coming soon.
+The first cut of this code was written by Van Riper and then I (Jono Bacon)
+rewrote it.
+
+## Usage
+
+Firstly, download the attendee list in plain text format. To do this:
+
+ 1. Go to http://www.eventbrite.com
+ 2. Click your name in the top-right corner of the page. Select *My Events*.
+ 3. Select your event from the list and in the Event Dashboard, select *Orders* from the *Manage Attendees* section.
+ 4. Use the Export combo box to select *Export to Text* and then save the page to your computer.
+
+You can now use this saved file as the *input file* in the commands below.
+
+### Generate a HTML list of attendees
+
+This is useful for embedding the list on your website.
+
+```
+python etools.py inputfile.txt -r
+```
+
+### Generate a list of email addresses for attendees
+
+This provides a list of email addresses, useful for reaching out to all attendees.
+
+```
+python etools.py inputfile.txt -e
+```
--- etools.py
+++ etools.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python2.4
-
 """Parse input file to extract and output data to files."""
 
 __author__ = 'jono@jonobacon.org (Jono Bacon)'
@@ -7,76 +5,111 @@ __author__ = 'vanriper@google.com (Van Riper)'
 
 import sys
 import re
+import string
 
-def main(argv):
-  """Parse input file to extract and output data to files.
+class Etools():
+    def __init__(self, argv):
+        self.args = sys.argv
 
-  argv must consist of three values: an input filename with the raw
-  tab delimited registration data, an output filename for a comma
-  separated list of email addresses, and an output filename for an
-  HTML list of just names and affiliations for attendees.
+        for a in self.args:
+            if a == "-r":
+                self.func_reg_html()
 
-  Args:
-    argv: Index 1 is the input file, e.g. cls-reg.txt.  Index 2 is the
-        output file for the email addresses, e.g. emails.txt. Index 3
-        is the output file for HTML unordered list, e.g. list.html.
-  """
-
-  if len(argv) < 4:
-    print 'Usage: etools.py [input-file] [output-emails] [output-html]'
-    print '       etools.py reg.txt emails.txt list.html'
-    sys.exit()
-
-  input_file = argv[1]
-  output_email_addresses = argv[2]
-  output_html_list = argv[3]
-
-  print 'Parsing %s to create %s and %s' % (input_file,
-                                       output_email_addresses,
-                                       output_html_list)
-  in_file = open(input_file, 'r')
-  print in_file
-  out_emails = open(output_email_addresses, 'w')
-  out_html = open(output_html_list, 'w')
-
-  names = []
-  emails = []
-
-  for line in in_file:
-    cells = line.split('\t')
-    print cells[12]
-    first_name = cells[12].strip().lower().title()
-    last_name = cells[13].strip().lower().title()
-    organization = cells[16].strip().lower().title()
-    role = cells[15].strip()
-    email = cells[14].strip()
-    if (first_name != last_name
-        and email != 'Email'
-        and re.match('.+@.+\..+', email)):
-      if not organization == "":
-          names.append('<strong>%s %s</strong> - <i>%s</i> - %s' % (first_name, last_name,
-                                      organization, role))
-      else:
-          names.append('<strong>%s %s</strong>' % (first_name, last_name))
-		  
-      emails.append(email.lower())
+            if a == "-e":
+                self.func_email_list()
 
 
-  prev_name = ''
-  names.sort()
-  out_html.write('<ul>\n')
-  for name in names:
-    if prev_name != name:
-      out_html.write('  <li>%s</li>\n' % name)
-    prev_name = name
-  out_html.write('</ul>\n')
+    def get_data_col(self, s):
+        """Return the column number for a given data type in the source file"""
 
-  prev_email = ''
-  emails.sort()
-  for email in emails:
-    if prev_email != email:
-      out_emails.write(email + '\n')
-    prev_email = email
+        num = 0
+
+        for i in self.data:
+            if i.rstrip() == s:
+                return num
+
+            num = num + 1
+
+    def func_reg_html(self):
+        """Create a HTML list of registered attendees, including first name,
+        last name, organization, and role."""
+
+        print "Generating list of registered attendees..."
+
+        self.load_source_file()
+
+        names = []
+        out_html = open("attendees-html-list.txt", 'w')
+
+        print self.in_file
+
+        for line in self.in_file:
+          cells = line.split('\t')
+          first_name = cells[self.get_data_col("First Name")].strip().lower().title()
+          last_name = cells[self.get_data_col("Last Name")].strip().lower().title()
+          organization = cells[self.get_data_col("Company / Organization")].strip().lower().title()
+          role = cells[self.get_data_col("Job Title")].strip().lower().title()
+
+          if (first_name != last_name):
+            if not organization == "":
+                names.append('<strong>%s %s</strong> - <i>%s</i> - %s' % (first_name, last_name,
+                                            organization, role))
+            else:
+                names.append('<strong>%s %s</strong>' % (first_name, last_name))
+
+
+        prev_name = ''
+        names.sort()
+        out_html.write('<ul>\n')
+        for name in names:
+          if prev_name != name:
+            out_html.write('  <li>%s</li>\n' % name)
+          prev_name = name
+        out_html.write('</ul>\n')
+
+        self.close_source_file()
+
+        print "...done."
+
+    def func_email_list(self):
+        """Generate a list of email addresses (useful for emailing registered
+        attendees)."""
+
+        print "Generating list of emails..."
+
+        self.load_source_file()
+
+        emails = []
+        out_emails = open("email-addresses.txt", 'w')
+
+        print self.in_file
+
+        for line in self.in_file:
+            cells = line.split('\t')
+            email = cells[self.get_data_col("Email Address")].strip().lower()
+            if re.match('.+@.+\..+', email):
+                emails.append(email.lower())
+
+        prev_email = ''
+        emails.sort()
+        for email in emails:
+          if prev_email != email:
+            out_emails.write(email + '\n')
+          prev_email = email
+
+        self.close_source_file()
+        print "...done."
+
+    def load_source_file(self):
+        input_file = sys.argv[1]
+
+        self.in_file = open(input_file, 'r')
+
+        self.data = self.in_file.readline().split('\t')
+
+    def close_source_file(self):
+        self.in_file.close()
+
 
 if __name__ == '__main__':
-  main(sys.argv)
+  e = Etools(sys.argv)
